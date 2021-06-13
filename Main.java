4e54dbf4b40b012cd27188da8ec05e34f26a0928
Shakiba <alipour.sh79@gmail.com>
2021-06-13 22:36:19 +0430

    recieve number of states and alphabets instead of their name

--- Main.java
+++ Main.java
@@ -1,8 +1,34 @@
 package com.company;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.*;
+import javafx.stage.Stage;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.io.IOException;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+        /*JFrame frame = new JFrame("Turing Machine");
+        frame.setSize(600, 400);
+        JLabel title = new JLabel("Turing Machine");
+        title.setBounds(300, 50, 100, 100);
+
+        frame.add(title);
+        frame.setVisible(true);
+        frame.show();*/
+    }
 
     public static void main(String[] args) {
-
+        Application.launch(args);
     }
 }
